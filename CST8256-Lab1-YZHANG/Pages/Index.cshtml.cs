fe75f5f661b2eacf20389004dbbde3b3c7eb0cb5
a42 <a42@P2C044>
2024-09-24 23:24:44 -0400

    refactor code-behind to use a single class and generic validation method

--- CST8256-Lab1-YZHANG/Pages/Index.cshtml.cs
+++ CST8256-Lab1-YZHANG/Pages/Index.cshtml.cs
@@ -1,135 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.Extensions.Logging; // Add this for logging
+using System.Collections.Generic;
+using System.Linq;
 
-// set up  a plain data model for the form that accepts four numbers
-public class InputModel
+[BindProperties]
+public class InputModel : PageModel
 {
-    public string? NumberOne { get; set; }
-
-    public string? NumberTwo { get; set; }
-
- 
-    public string? NumberThree { get; set; }
-
-    
-    public string? NumberFour { get; set; }
-
-}
-public class IndexModel : PageModel
-{
-    // Using the BindProperty directive corresponding data from page form is bind to an instance of the input model
-
-    [BindProperty]
-    public InputModel InputModel { get; set; } = new InputModel();
+    public string NumberOne { get; set; } = string.Empty;
+    public string NumberTwo { get; set; } = string.Empty;
+    public string NumberThree { get; set; } = string.Empty;
+    public string NumberFour { get; set; } = string.Empty;
 
     public string? StatisticsResult { get; private set; }
 
+    public int counter = 0;
 
-    // validate input data
-    public void OnPost()
+    // Validate input data
+    List<string> fieldNames = new List<string> { "NumberOne", "NumberTwo", "NumberThree", "NumberFour" };
+    private List<double> ValidateInput(List<string> inputs)
     {
-
-        
-        // implement custom validation logic
-        // first check that at least two numbers are entered
-        double one = 0.0;
-        double two = 0.0;
-        double three = 0.0;
-        double four = 0.0;
-        // use list instead of array for unknown size of enums
-        List<double> doubles = new List<double>();
-        int counter = 0;
-        // NOTE: a generic method with three parameters can be used to dynamically 
-        // validate input method(input string, output double, errorMessage string)
-
-        if (double.TryParse(InputModel.NumberOne, out one)) 
+        List<double> result = new();
+        for (int i = 0; i < inputs.Count; i++)
         {
-            counter++;
-            doubles.Add(one);
-        }
-        else
-        {
-            ModelState.AddModelError(nameof(InputModel.NumberOne), "Must be a number");
-        }
-        if (double.TryParse(InputModel.NumberTwo, out two))
-        {
-            counter++;
-            doubles.Add(two);
-        }
-        else
-        {
-            ModelState.AddModelError(nameof(InputModel.NumberTwo), "Must be a number");
-        }
-        if (double.TryParse (InputModel.NumberThree, out three))
-        {
-            counter++;
-            doubles.Add(three);
-        }
-        else
-        {
-            ModelState.AddModelError(nameof(InputModel.NumberThree), "Must be a number");
-        }
-        if (double.TryParse(InputModel.NumberFour, out four))
-        {
-            counter++;
-            doubles.Add(four);
-        }
-        else
-        {
-            ModelState.AddModelError(nameof(InputModel.NumberFour), "Must be a number");
-        }
-        // TODO : has to be a way NOT to hard code the counter logic
-
-        // debugger CONFIRMED correct key value binding for each field
-        foreach (var entry in ModelState)
-        {
-            foreach (var error in entry.Value.Errors)
+            if (!string.IsNullOrEmpty(inputs[i]))
             {
-                Console.WriteLine($"Field: {entry.Key}, Error: {error.ErrorMessage}");
+                double value;
+                if (double.TryParse(inputs[i], out value))
+                {
+                    counter += 1;
+                    result.Add(value);
+                }
+                else
+                {
+                    // Console.WriteLine($"the error for {fieldNames[i]}  to be added");
+                    ModelState.AddModelError(fieldNames[i], "Must be a number");
+                }
             }
         }
-
-        // debugger end
-
-        // debugger ModelState is passed to view correctly 
-        if (!ModelState.IsValid)
-        {
-            Console.WriteLine("ModelState is invalid");  // This should print when there's an error.
-        }
-
-        // handle error where entire form has fewer than two numbers
-        if (counter <2)
-        {
-            ModelState.AddModelError(String.Empty, "Must enter at least two numbers.");
-
-            return;
-        }
-
-        
-
-        // do calculation on valid input
-        else
-        {
-
-            StatisticsResult = $"You have entered {counter} numbers.<ul>" +
-            $"<li>Maximum is {doubles.Max()}</li>" +
-            $"<li>Minimum is {doubles.Min()}</li>" +
-            $"<li>Total is {doubles.Sum()}</li>" +
-            $"<li>Average is {doubles.Average()}</li>" +
-            "</ul>";
-
-        }
-
-
-
+        return result;
     }
 
+    public void OnPost()
+    {
+        counter = 0; // Reset counter on each submission
+
+        // Collect inputs from bound properties
+        List<string> userInputs = new List<string> {NumberOne, NumberTwo, NumberThree, NumberFour };
+
+        List<double> doubles = ValidateInput(userInputs );
+
+        // Handle error where fewer than two numbers are entered
+        if (counter < 2)
+        {
+            ModelState.AddModelError(string.Empty, "Must enter at least two numbers.");
+            return; // Return early if not enough valid inputs
+        }
+
+        //foreach (var entry in ModelState)
+        //{
+        //    foreach (var error in entry.Value.Errors)
+        //    {
+        //        Console.WriteLine($"Field: {entry.Key}, Error: {error.ErrorMessage}");
+        //    }
+        //}
+
+        // Perform calculations on valid input
+        StatisticsResult = $"You have entered {counter} numbers.<ul>" +
+                           $"<li>Maximum is {doubles.Max()}</li>" +
+                           $"<li>Minimum is {doubles.Min()}</li>" +
+                           $"<li>Total is {doubles.Sum()}</li>" +
+                           $"<li>Average is {doubles.Average()}</li>" +
+                           "</ul>";
+    }
 }
-
-
-
